3c0a93c492892fc092402fe38d8a7ab237ead9e2
farkhondeh-zeinaly <154916780+farkhondeh-zeinaly@users.noreply.github.com>
2023-12-26 21:47:26 +0330

    Add files via upload

---
+++ ABTProject/ABTProject/GraphColoring.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ABTProject
+{
+    public class GraphColoring
+    {
+        int _vertexNo;
+
+        public GraphColoring(int vertexNo)
+        {
+            _vertexNo = vertexNo;
+        }
+
+        int[] color;
+
+        public bool CheckLoacalView(int v, int[][] graph, int[] color, int c)
+
+        {
+            for (int i = 0; i < _vertexNo; i++)
+            {
+                if (graph[v][i] == 1 && c == color[i])
+                {
+                    Console.WriteLine("     BackTrack...");
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        public bool HandleOk(int[][] graph, int m, int[] color, int v)
+        {
+            if (v == _vertexNo)
+            {
+                return true;
+            }
+
+            Console.WriteLine("Checking vertex " + v);
+
+            for (int c = 1; c <= m; c++)
+            {
+                Console.WriteLine("     Checking color " + c + " for Vertex " + v);
+                if (CheckLoacalView(v, graph, color, c))
+                {
+                    color[v] = c;
+                    Console.WriteLine("         Color " + c + " assigned for Vertex " + v + Environment.NewLine);
+
+                    if (HandleOk(graph, m, color, v + 1))
+                    {
+                        return true;
+                    }
+
+                    color[v] = 0;
+                }
+            }
+
+            return false;
+        }
+
+        public bool DoColoring(int[][] graph, int m)
+        {
+            color = new int[_vertexNo];
+
+            for (int i = 0; i < _vertexNo; i++)
+            {
+                color[i] = 0;
+            }
+
+            if (!HandleOk(graph, m, color, 0))
+            {
+                Console.WriteLine("Solution does not exist");
+                return false;
+            }
+
+            PrintSolution(color);
+            return true;
+        }
+
+        public void PrintSolution(int[] color)
+        {
+            Console.WriteLine("Solution:");
+
+            for (int i = 0; i < _vertexNo; i++)
+            {
+                Console.WriteLine("     Vertex " + i + " -> Color: " + color[i] + " ");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
---
+++ ABTProject/ABTProject/Program.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ABTProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("----------------------------------");
+            Console.WriteLine("--- ABT graph coloring problem ---");
+            Console.WriteLine("----------------------------------");
+            Console.WriteLine();
+
+            int vertexCount = 0;
+
+            while (vertexCount < 1)
+            {
+                Console.WriteLine("Vertexes count:");
+                try
+                {
+                    vertexCount = Convert.ToInt32(Console.ReadLine());
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Invalid input!");
+                }
+            }
+
+            int[][] graph = new int[vertexCount][];
+            for (int i = 0; i < graph.Length; i++)
+            {
+                graph[i] = new int[vertexCount];
+            }
+
+
+            Console.WriteLine("If vertex i has link with vertex j enter 1 othervise 0:");
+
+            for (int i = 0; i < vertexCount; i++)
+            {
+                for (int j = 0; j < vertexCount; j++)
+                {
+                    if (i == j)
+                    {
+                        graph[i][j] = 0;
+                    }
+                    else
+                    {
+                        int hasLink = -1;
+                        while (!(hasLink == 0 || hasLink == 1))
+                        {
+                            Console.WriteLine(i + " -> " + j + "  ?");
+                            try
+                            {
+                                hasLink = Convert.ToInt32(Console.ReadLine());
+                                if (hasLink >= 0 && hasLink <= 1)
+                                {
+                                    graph[i][j] = hasLink;
+                                }
+                            }
+                            catch (Exception)
+                            {
+                                Console.WriteLine("Invalid input!");
+                            }
+                        }
+                    }
+                }
+            }
+
+
+            Console.WriteLine("Graph:");
+
+            for (int i = 0; i < vertexCount; i++)
+            {
+                Console.Write("      |");
+
+                for (int j = 0; j < vertexCount; j++)
+                {
+                    Console.Write(graph[i][j] + "|");
+                }
+
+                Console.Write(Environment.NewLine);
+            }
+
+            int colorsCount = 0;
+
+            while (colorsCount < 1)
+            {
+                Console.WriteLine("Colors count:");
+                try
+                {
+                    colorsCount = Convert.ToInt32(Console.ReadLine());
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Invalid input!");
+                }
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Operation starts...");
+
+
+            GraphColoring coloring = new GraphColoring(vertexCount);
+
+
+            //int[][] graph = new int[][]
+            //{
+            //             new int[] {0,1,1,1},
+            //             new int[] {1,0,1,0},
+            //             new int[] {1,1,0,1},
+            //             new int[] {1,0,1,0},
+            //};
+
+            coloring.DoColoring(graph, colorsCount);
+
+            Console.ReadLine();
+        }
+    }
+}
